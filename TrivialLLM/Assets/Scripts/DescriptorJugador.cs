aed7469a31b076ff73dc9bfcae6d66e7af4f2f57
oscarpb99 <oscarpedregal2299@gmail.com>
2026-03-18 00:18:05 +0100

    Añadido menú principal, de configuración de partida y escena final

---
+++ TrivialLLM/Assets/Scripts/DescriptorJugador.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class DescriptorJugador
+{
+    public bool esHumano;
+    //Si no es humano
+    public AIService.Models modelo;
+    public string prompt;
+}
--- TrivialLLM/Assets/Scripts/GameManager.cs
+++ TrivialLLM/Assets/Scripts/GameManager.cs
@@ -1,9 +1,27 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
+
+    public TextMeshProUGUI textoNumHumanos;
+    public TextMeshProUGUI textoNumLLMS;
+
+    public GameObject panel;
+    public TMP_Dropdown model;
+    public TMP_InputField promptText;
+
+    public int numMaxJugadores=1;
+    public int numMaxLLMS = 3;
+
+    private int numJug;
+    private int numLLMS;
+
+    public List<DescriptorJugador> descriptorJug;
 
     // Modos de juego posible
     public enum GameMode { HumanGame, AIGame }
@@ -16,6 +34,9 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+            numJug = 0;
+            numLLMS = 0;
+            descriptorJug = new List<DescriptorJugador>();
         }
         else
         {
@@ -48,4 +69,128 @@ public class GameManager : MonoBehaviour
         SetGameMode(GameMode.AIGame);
         SceneManager.LoadScene("IAGameScene");
     }
+
+    public void addHuman()
+    {
+        if (numJug < numMaxJugadores)
+        {
+            descriptorJug.Add(new DescriptorJugador { esHumano = true });
+            numJug++;
+            textoNumHumanos.text = numJug.ToString();
+        }
+    }
+    public void addLLM()
+    {
+        if (numLLMS < numMaxLLMS)
+        {
+            //descriptorJug.Add(new DescriptorJugador { esHumano = false, modelo=model, prompt=promptDes });
+            numLLMS++;
+            textoNumLLMS.text = numLLMS.ToString();
+            if(!panel.activeSelf && numLLMS > 0)
+            {
+                panel.SetActive(true);
+            }
+          
+        }
+    }
+
+    public void registrarLLM()
+    {
+        if (numLLMS < numMaxLLMS)
+        {
+            string modelo = model.options[model.value].text;
+            AIService.Models modeloSeleccionado;
+            switch (modelo)
+            {
+                case "Gemini":
+                    modeloSeleccionado = AIService.Models.Gemini; 
+                    break;
+                case "Copilot":
+                    modeloSeleccionado = AIService.Models.Copilot;
+                    break;
+                case "ChatGPT":
+                    modeloSeleccionado = AIService.Models.ChatGPT;
+                    break;
+                case "Azure":
+                    modeloSeleccionado = AIService.Models.Azure;
+                    break;
+                default:
+                    modeloSeleccionado = AIService.Models.Copilot;
+                    break;
+                    
+            }
+            descriptorJug.Add(new DescriptorJugador { esHumano = false, modelo =modeloSeleccionado, prompt=promptText.text });
+            // Debug del último jugador agregado
+            DescriptorJugador ultimo = descriptorJug[descriptorJug.Count - 1];
+            Debug.Log("Último LLM agregado -> Modelo: " + ultimo.modelo
+                      + ", Prompt: " + ultimo.prompt
+                      + ", EsHumano: " + ultimo.esHumano);
+
+        }
+    }
+
+    public void quitarHum()
+    {
+        if (numJug > 0)
+        {
+            int i = descriptorJug.Count - 1;
+            bool enc = false;
+            while(!enc && i >= 0) {
+                if (descriptorJug[i].esHumano)
+                {
+                    enc = true;
+                    descriptorJug.RemoveAt(i);
+                    numJug--;
+                    textoNumHumanos.text= numJug.ToString();
+                }
+                else
+                {
+                    i--;
+                }
+            }
+        }
+    }
+    public void quitarLLM()
+    {
+        if (numLLMS > 0)
+        {
+            int i = descriptorJug.Count - 1;
+            bool enc = false;
+            while (!enc && i >= 0)
+            {
+                if (!descriptorJug[i].esHumano)
+                {
+                    enc = true;
+                    descriptorJug.RemoveAt(i);
+                    numLLMS--;
+                    textoNumLLMS.text= numLLMS.ToString();
+                    if (numLLMS <= 0)
+                    {
+                        panel.SetActive(false);
+                    }
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            if(!enc &&  numLLMS > 0)
+            {
+                numLLMS = 0;
+                textoNumLLMS.text = numLLMS.ToString();
+                panel.SetActive(false);
+            }
+        }
+    }
+
+    public int getNumHuman()
+    {
+        return numJug;
+    }
+
+    public int getNumLLMS()
+    {
+        return numLLMS;
+    }
+    
 }
