c3cb8404cee8e639d6193fa3c32dad30fa920c67
GonzaloFdezMoreno <48910540+GonzaloFdezMoreno@users.noreply.github.com>
2026-04-13 18:25:20 +0200

    Tirada de dados

---
+++ TrivialLLM/Assets/Scripts/DiceTrows.cs
@@ -0,0 +1,63 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceTrows : MonoBehaviour
+{
+    [SerializeField]
+    Text resultado;
+    [SerializeField]
+    Image diceImage;
+    [SerializeField]
+    Sprite[] dicefaces;
+
+    bool throwed=false;
+    int startFace;
+    float countToChange = 0.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startFace = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!throwed)
+        {
+            if (startFace >= dicefaces.Length)
+            {
+                startFace = 0;
+            }
+            else
+            {
+                startFace++;
+            }
+            if (countToChange > 0.2f)
+            {
+                diceImage.sprite = dicefaces[startFace];
+                countToChange = 0.0f;
+            }
+            else
+            {
+                countToChange += Time.deltaTime;
+            }
+        }
+    }
+    public void releaseNumber()
+    {
+        if (!throwed)
+        {
+            throwed = true;
+            int diceNum = Random.Range(1, 7);
+            //Guardar el nº para el avance del jugador
+            resultado.text = "Avanzas " + diceNum.ToString() + " casillas";
+            diceImage.sprite= dicefaces[diceNum-1];
+        }
+        else
+        {
+            throwed = false;
+        }
+    }
+}
